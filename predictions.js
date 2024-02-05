d12486666b3bf3fa7a620ca3e037ff87f74398e5
Core447 <jmulmann@protonmail.com>
2024-02-05 18:12:26 +0100

    Add no connectin to backend error msg

--- predictions.js
+++ predictions.js
@@ -1,3 +1,8 @@
+const pred_words = document.querySelectorAll(".pred-word");
+const no_backend_error = document.querySelector(".no-backend-error");
+setShowBackendError(true);
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const inputElement = document.querySelector('.nwp-input');
     
@@ -23,10 +28,14 @@ async function getNextWords(text) {
         }
 
         const data = await response.json();
+        
+        setShowBackendError(false);
+
         return data;
         // Update the DOM or take other actions here with data.next_word
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
+        setShowBackendError(true);
     }
 
 }
@@ -39,4 +48,25 @@ function showNextWords(data) {
     for (let i = 0; i < predWords.length; i++) {
         predWords[i].textContent = data[`word${i+1}`][0];
     }
+}
+
+function setShowBackendError(show){
+    // Set vis of no_backend_error
+    if (show) {
+        no_backend_error.style.display = "block";
+    } else {
+        no_backend_error.style.display = "none";
+    }
+
+    // Set vis of pred_words
+    if (show) {
+        for (let i = 0; i < pred_words.length; i++) {
+            pred_words[i].style.display = "none";
+        }
+    } else {
+        for (let i = 0; i < pred_words.length; i++) {
+            pred_words[i].style.display = "inline";
+        }
+    }
+
 }
