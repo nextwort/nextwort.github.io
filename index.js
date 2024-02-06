72cd40bb72976e9e774f93d1dac56daed9c4c3e9
Core447 <jmulmann@protonmail.com>
2024-02-06 13:48:18 +0100

    Add word completetion models to dropdown

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-async function populateModelDropdown() {
-    try {
-        const response = await fetch('http://localhost:13232/get_next_word_models', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ msg: "text" }), // Make sure the body matches your server's expected format
-        });
-        
+async function populateModelDropdown(url, dropdownId) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
     const data = await response.json();
-      const dropdown = document.getElementById('next-word-model-dropdown');
-      
-      // Clear the dropdown before adding the new model names
-      dropdown.innerHTML = '';
-  
-      data.model_names.forEach(modelName => {
-        const option = document.createElement('option');
-        option.text = modelName;
-        dropdown.add(option);
-      });
-    } catch (error) {
-      console.error('Error fetching model names:', error);
-    }
+    const dropdown = document.getElementById(dropdownId);
+
+    // Clear the dropdown before adding the new model names
+    dropdown.innerHTML = '';
+
+    data.model_names.forEach(modelName => {
+      const option = document.createElement('option');
+      option.text = modelName;
+      dropdown.add(option);
+    });
+  } catch (error) {
+    console.error('Error fetching model names:', error);
   }
-  
-  // Call the async function to populate the dropdown
-  populateModelDropdown();
+}
+
+// Call the async function to populate the dropdown
+populateModelDropdown('http://localhost:13232/get_next_word_models', 'next-word-model-dropdown');
+populateModelDropdown('http://localhost:13232/get_complete_word_models', 'word-comp-model-dropdown');
