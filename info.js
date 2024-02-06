5b007d9ae0afb8c704358aee9f9bd9b9829bb562
Core447 <jmulmann@protonmail.com>
2024-02-06 16:19:02 +0100

    Add info buttons to model selectors

---
+++ info.js
@@ -0,0 +1,13 @@
+const nextWordInfoButton = document.getElementById("next-word-info-button");
+const wordCompInfoButton = document.getElementById("word-comp-info-button");
+
+nextWordInfoButton.addEventListener("click", function() {
+  const dropdownValue = document.getElementById("next-word-model-dropdown").value;
+  window.location.href = "/info/" + dropdownValue + ".html"
+});
+
+wordCompInfoButton.addEventListener("click", function() {
+  const dropdownValue = document.getElementById("word-comp-model-dropdown").value;
+  window.location.href = "/info/" + dropdownValue + ".html"
+})
+
