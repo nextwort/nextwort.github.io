2d5db7896106fc108501170c036984e5ae908ed3
Core447 <jmulmann@protonmail.com>
2024-02-08 19:43:40 +0100

    Revert last commit

--- screens/chat/chat-padding.js
+++
@@ -1,9 +0,0 @@
-// Function to update the padding-bottom of the body
-function updateBodyPadding() {
-    var bottomAreaHeight = document.querySelector('.bottom-area').offsetHeight;
-    document.body.style.paddingBottom = bottomAreaHeight + 'px';
-}
-
-// Call the function initially and whenever the window is resized
-updateBodyPadding();
-window.addEventListener('resize', updateBodyPadding);
